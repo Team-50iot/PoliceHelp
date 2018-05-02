c4bca89ee9c44c7c386c43dc3a9272d68b7b79c4
Predator12 <panchushak@gmail.com>
2018-05-02 20:36:30 +0300

    add json

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask , jsonify
 from DBcm import UseDatabase
 import mysql.connector
-
+from flask import Flask, request,jsonify
+from flask_restful import Resource, Api
+from sqlalchemy import create_engine
 app = Flask(__name__)
 
 
@@ -14,16 +16,19 @@ conn = mysql.connector.connect(user="root",
 
 @app.route('/', methods=['GET'])
 def home():
-    script = """SELECt time from data"""
+    script = """SELECT  id , time  , number from data"""
     cursor = conn.cursor()
 
     cursor.execute(script)
-    rv = cursor.fetchone()
+    row_headers = [x[0] for x in cursor.description]
+    rv = cursor.fetchall()
+    json_data = []
+    for result in rv:
+        json_data.append(dict(zip(row_headers, result)))
+    return jsonify(json_data)
 
-
-    return rv
 
 
 
 if __name__ == '__main__':
-    app.run(host='192.168.137.154', port=8000,debug=True)
+    app.run(debug=True)
