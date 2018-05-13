cf3536c042de5cb2023575563cfb750a6b63d595
Predator12 <panchushak@gmail.com>
2018-05-13 12:17:06 +0300

    fix database connect

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,38 @@
 import mysql.connector
 from flask import Flask,jsonify,redirect,render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,template_folder='Our_WebSite')
 
 
-conn = mysql.connector.connect(user="root",
-                               password="11111111",
-                               host="localhost",
+user, password = '1pavel1', 'ppm021999'
+host = '1pavel1.mysql.pythonanywhere-services.com'
+db = '1pavel1$PoliceHelp' # dbFlask was created as a PythonAnywhere MySQL database
 
-                               database="mydb")
+# connection string: mysql://user:pword@host/db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{0}:{1}@{2}/{3}'.format(user, password, host, db)
+db = SQLAlchemy(app)
+
+
+
+'''SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{1pavel1}:{ppm021999}@{1pavel1.mysql.pythonanywhere-services.com}/{PoliceHelp}".format(
+    username="1pavel1",
+    password="ppm021999",
+    hostname="1pavel1.mysql.pythonanywhere-services.com",
+    databasename="PoliceHelp",
+
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+conn = SQLAlchemy(app)'''
+
+'''conn = mysql.connector.connect(user="1pavel1",
+                               password="ppm021999",
+                               host="1pavel1.mysql.pythonanywhere-services.com",
+                               database="1pavel1$PoliceHelp")'''
 
 
 
@@ -35,8 +58,8 @@ def vkontakte() -> 'html':
 
 @app.route('/db', methods=['GET'])
 def home():
-    script = """SELECT  id , time  , number from data"""
-    cursor = conn.cursor()
+    script = """SELECT  id , time  , number from info"""
+    cursor = db.cursor()
 
     cursor.execute(script)
     row_headers = [x[0] for x in cursor.description]
